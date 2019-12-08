dbf1ac5e034231769954ee76b6f9447c0b3584b6
Richard Harris <rjh@netcraft.com>
2019-12-08 12:18:21 +0000

    Add q8 and new util module.

---
+++ q8.py
@@ -0,0 +1,69 @@
+from enum import Enum
+from util import slurp, split_every
+from itertools import starmap
+
+def part1():
+    """
+    image is split into layers of width*height
+    find layer with smallest number of zeros
+    return number of 1s * number of 2s
+    """
+    image = slurp('q8_input')
+    width, height = 25, 6
+    layers = split_every(width * height, image)
+    fewestZeroLayer = min(layers, key=lambda l: l.count('0'))
+    print(fewestZeroLayer.count('1') * fewestZeroLayer.count('2'))
+
+def part2():
+    """
+    stack the layers: first layer in front, last layer in back
+    0 = black, 1 = white, 2 = transparent
+    therefore black/white overlap everything, and transparent overlaps nothing
+    """
+    image = slurp('q8_input')
+    width, height = 25, 6
+    combined = combineLayers(image, width, height)
+    print(layerToImage(combined, width))
+
+def part2_test():
+    test = "0222112222120000"
+    print(''.join(combineLayers(test, 2, 2)))
+
+class Color(Enum):
+    BLACK = '0'
+    WHITE = '1'
+    TRANSPARENT = '2'
+
+def combineLayers(image, width, height):
+    layers = split_every(width * height, image)
+
+    # Start with transparent canvas
+    canvas = Color.TRANSPARENT.value * (width * height)
+
+    def applyPixel(oldValue, newValue):
+        """ apply newValue behind oldValue """
+        if oldValue == Color.TRANSPARENT.value:
+            return newValue
+        else:
+            return oldValue
+
+    for layer in layers:
+        canvas = starmap(applyPixel, zip(canvas, layer))
+
+    return canvas
+
+def layerToImage(image, width):
+    """
+    images are stored as a long string of chars. this function adds newlines
+    so the image can be printed and viewed by a human, and removes transparent
+    pixels
+    """
+    image = '\n'.join(''.join(chunk) for chunk in split_every(width, image))
+    # Remove transparent pixels
+    image = image.replace('2', ' ')
+    # Make image more readable
+    image = image.replace('0', '.')
+    image = image.replace('1', '#')
+    return image
+
+part2()
---
+++ util.py
@@ -0,0 +1,18 @@
+from itertools import islice
+
+def split_every(n, iterable):
+    i = iter(iterable)
+    piece = list(islice(i, n))
+    while piece:
+        yield piece
+        piece = list(islice(i, n))
+
+def slurp(filename):
+    with open(filename, 'r') as fh:
+        return fh.read().rstrip('\n')
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
