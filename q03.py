e848a14a180e6466ea1a70c99ed90351c4808c2c
Richard Harris <rjh@netcraft.com>
2019-12-13 21:33:29 +0000

    Merge q03_part2 into q03

--- q03.py
+++ q03.py
@@ -1,8 +1,7 @@
 "Day 3 - Crossed Wires"
-import copy
 import enum
 import unittest
-from typing import Iterable, Set, Tuple
+from typing import Iterable, Tuple, Dict, Optional, List
 from util import read_lines
 
 Point = Tuple[int, int]
@@ -15,29 +14,41 @@ class Dir(enum.Enum):
     U = (0, 1)
     D = (0, -1)
 
-def wire_points(wire_string: str) -> Set[Point]:
+
+def find_intersections(wires: List[Dict[Point, int]]):
     """
-    given a wire string such as 'D3,R1,U2...' return a set of tuples
-    marking points the wire covers.
+    Given a list of wires (dicts of point -> steps taken)
+    Return the points present in all wires, i.e. the intersections
+    """
+    if not wires:
+        return set()
+
+    intersections = wires[0].keys()
+    for wire in wires[1:]:
+        intersections &= wire.keys()
+
+    return intersections
+
+
+def wire_points(wire_string: str) -> Dict[Point, int]:
+    """
+    Given a wire string such as 'D3,R1,U2...' return a dict mapping
+    each point the wire covers, with the minimum steps taken before
+    the wire reaches that point.
     """
     pos = (0, 0)
-    points = set()
+    points: Dict[Point, int] = dict()
+    steps = 0
     for part in wire_string.split(','):
         direction = Dir[part[0]].value
         distance = int(part[1:])
         for _ in range(distance):
+            steps += 1
             pos = (pos[0] + direction[0], pos[1] + direction[1])
-            points.add(copy.copy(pos))
+            if pos not in points:
+                points[pos] = steps
 
     return points
-
-
-def find_intersections(strings: Iterable[str]) -> Set[Point]:
-    """
-    given multiple strings, return the points that occur in all
-    strings
-    """
-    return set.intersection(*map(wire_points, strings))
 
 
 def manhattan_dist_from_origin(pos: Point) -> int:
@@ -55,9 +66,26 @@ def part1(wire_strings: Iterable[str]) -> int:
     Given a list of wire strings, return the Manhattan distance from
     the origin to the closest intersection of all strings.
     """
-    crosses = find_intersections(wire_strings)
+    wires = list(map(wire_points, wire_strings))
+    crosses = find_intersections(wires)
     return manhattan_dist_from_origin(find_closest(crosses))
 
+
+def part2(wire_strings: Iterable[str]) -> Optional[int]:
+    """
+    Given a list of wire strings, return the minimum combined number
+    of steps along each strings' paths before an intersection is
+    reached.
+    """
+    wires = list(map(wire_points, wire_strings))
+    lowest = None
+
+    for crossing in find_intersections(wires):
+        steps = sum(wire[crossing] for wire in wires)
+        if lowest is None or steps < lowest:
+            lowest = steps
+
+    return lowest
 
 class TestQ3(unittest.TestCase):
     def test_part1_basic(self):
@@ -67,12 +95,14 @@ class TestQ3(unittest.TestCase):
         ]
         self.assertEqual(part1(wires), 6)
 
+
     def test_part1_example1(self):
         wires = [
             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
             "U62,R66,U55,R34,D71,R55,D58,R83",
         ]
         self.assertEqual(part1(wires), 159)
+
 
     def test_part1_example2(self):
         wires = [
@@ -81,6 +111,28 @@ class TestQ3(unittest.TestCase):
         ]
         self.assertEqual(part1(wires), 135)
 
+
     def test_part1(self):
         wires = read_lines("inputs/q03")
         self.assertEqual(part1(wires), 1225)
+
+
+    def test_part2_example1(self):
+        wires = [
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+        ]
+        self.assertEqual(part2(wires), 610)
+
+
+    def test_part2_example2(self):
+        wires = [
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+        ]
+        self.assertEqual(part2(wires), 410)
+
+
+    def test_part2(self):
+        wires = read_lines("inputs/q03")
+        self.assertEqual(part2(wires), 107036)
