c91cb32436ffbc9269205f74669e0e23b47ca603
Richard Harris <rjh@netcraft.com>
2019-12-15 21:15:48 +0000

    Day 6: add unit tests, tidy, fix unclosed file warning

--- q06.py
+++ q06.py
@@ -1,21 +1,27 @@
-from collections import defaultdict
-from copy import deepcopy
-from util import path_find
+'Day 6 - Universal Orbit Map'
+import collections
+import copy
 import math
+import unittest
+import util
 
 def loadOrbits(filename):
     """ load orbits from text file """
-    graph = defaultdict(set)
+    graph = collections.defaultdict(set)
 
-    for line in open(filename, 'r'):
-        line = line.rstrip('\n')
-        orbitee, orbiter = line.split(')')
-        graph[orbitee].add(orbiter)
+    with open(filename, 'r') as fh:
+        for line in fh:
+            line = line.rstrip('\n')
+            orbitee, orbiter = line.split(')')
+            graph[orbitee].add(orbiter)
 
     return graph
 
+
 def countOrbits(graph, node='COM', depth=0):
     """ return the number of direct and indirect orbits starting at node """
+    graph = collections.defaultdict(set, graph)
+
     direct, indirect = 0, 0
 
     for orbiter in graph[node]:
@@ -28,22 +34,22 @@ def countOrbits(graph, node='COM', depth=0):
 
     return direct, indirect
 
+
 def totalOrbits(graph):
     """ return the total number of orbits in the graph """
     direct, indirect = countOrbits(graph)
     return direct + indirect
 
-def part1():
-    print(totalOrbits(loadOrbits("q6_input")))
 
 def makeBidirectional(graph):
     """ convert a unidirectional graph to a bidirectional by adding B->A
         for every edge A->B in the graph """
-    new = deepcopy(graph)
+    new = collections.defaultdict(set, copy.deepcopy(graph))
     for fromNode, toNodes in graph.items():
         for toNode in toNodes:
             new[toNode].add(fromNode)
     return new
+
 
 def findRoute(graph, start='YOU', goal='SAN'):
     """ find minimal route from start to goal along graph """
@@ -54,7 +60,8 @@ def findRoute(graph, start='YOU', goal='SAN'):
     def neighbourFunc(node):
         return neighboursOf[node]
 
-    return path_find(start, goal, neighbourFunc)
+    return util.path_find(start, goal, neighbourFunc)
+
 
 def minimumTransfersRequired(graph):
     route = findRoute(graph)
@@ -62,5 +69,58 @@ def minimumTransfersRequired(graph):
     # This is two transfers (A->B, B->C)  so we need to remove 3 from the total
     return len(route) - 3
 
-graph = loadOrbits("inputs/q06")
-print(minimumTransfersRequired(graph))
+
+class TestQ6(unittest.TestCase):
+
+    def test_count_basic(self):
+        graph = {
+            'COM': { 'BBB' },
+        }
+        self.assertEqual(totalOrbits(graph), 1)
+
+        graph = {
+            'COM': { 'BBB' },
+            'BBB': { 'CCC' },
+        }
+        # AAA -> BBB, AAA -> CCC, BBB -> CCC
+        self.assertEqual(totalOrbits(graph), 3)
+
+
+    def test_count_example(self):
+        graph = {
+            'COM': { 'B' },
+            'B': { 'C', 'G' },
+            'C': { 'D' },
+            'D': { 'E', 'I' },
+            'E': { 'F', 'J' },
+            'G': { 'H' },
+            'J': { 'K' },
+            'K': { 'L' },
+        }
+        self.assertEqual(totalOrbits(graph), 42)
+
+
+    def test_part1(self):
+        graph = loadOrbits("inputs/q06")
+        self.assertEqual(totalOrbits(graph), 273985)
+
+
+    def test_transfer_example(self):
+        graph = {
+            'COM': { 'B' },
+            'B': { 'C', 'G' },
+            'C': { 'D' },
+            'D': { 'E', 'I' },
+            'E': { 'F', 'J' },
+            'G': { 'H' },
+            'I': { 'SAN' },
+            'J': { 'K' },
+            'K': { 'L', 'YOU' },
+        }
+        self.assertEqual(minimumTransfersRequired(graph), 4)
+
+
+    def test_part2(self):
+        graph = loadOrbits("inputs/q06")
+        self.assertEqual(minimumTransfersRequired(graph), 460)
+        
